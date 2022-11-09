f5d05d28f9da332526ceb8fdebf11a829e77db27
Raissa <raissamm01@hotmail.com>
2022-11-09 08:21:31 -0300

    Class Tableau

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-#Simplex
-# test
+import numpy as np
+
+# The tableau method for simplex 
+class Tableau:
+    def __init__(self):
+        self.c = []                     # Objective function
+        self.A = []                     # Restrictions
+        self.b = []                     # Restrictions
+        self.optimal_solution = 0       # Maximum optimal value
+        self.base_columns = []          # Bases index
+        self.dimension = (0,0)          # A's dimension
+        self.identification = ''        # Optimal, unbounded or impossible
+
