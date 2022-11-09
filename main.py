eb021c0202f62a4687210ee25a2fc7ca73ca9105
Raissa <raissamm01@hotmail.com>
2022-11-09 13:36:41 -0300

    Testing canonical form

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 
 # The tableau method for simplex 
@@ -23,21 +24,39 @@ def rounding_to_zero(value):
 # Converting an LP into canonical form
 def canonical_form(self):
     # Make all the basic variables pivots values equal to 1 and the other variables 0 (includind c inputs)
-    for i in range(self.dimensions[0]):                      # For each basic variable
-        if(self.A[i][self.base_columns[i]] != 0):            # Pivot is not null
-            aux = self.A[i][self.base_columns[i]].copy()     # Value that annuls the variable when divided
-            self.A[i,:] /= aux                               # Make pivot value equals to 1
-            self.b[i] /= aux                                 # Vector b is also divided
-        for j in range(self.dimension[0]):                   # For each line
-            if (i != j):                                     # It's not the basic variable line
-                aux = self.A[j][self.base_columns[i]].copy() # Value that should be null
-                self.A[j,:] -= aux * self.A[i,:]             # Subtract all the line
-                self.b[j] -= aux * self.b[i]                 # Vector b is also subtracted
-        aux = self.c[self.base_columns[i]]                   # c input must also be null
-        self.c -= aux * self.A[i,:]                          # Substract all the vector c
-        self.optimal_solution -= aux * self.b[i]             # Substract the optimal value with respect to vector b
-    vfunc = np.vectorize(rounding_to_zero)                         # Applies the function in parameter in all the vector's input
-    self.A = vfunc(self.A)                                   # (set very low values to zero)
+    for i in range(self.dimension[0]):                            # For each basic variable
+        if(self.A[i][self.base_columns[i]] != 0):                 # Pivot is not null
+            aux = copy.deepcopy(self.A[i][self.base_columns[i]])  # Value that annuls the variable when divided
+            self.A[i,:] /= aux                                    # Make pivot value equals to 1
+            self.b[i] /= aux                                      # Vector b is also divided
+        for j in range(self.dimension[0]):                        # For each line
+            if (i != j):                                          # It's not the basic variable line
+                aux = self.A[j][self.base_columns[i]].copy()      # Value that should be null
+                self.A[j,:] -= aux * self.A[i,:]                  # Subtract all the line
+                self.b[j] -= aux * self.b[i]                      # Vector b is also subtracted
+        aux = self.c[self.base_columns[i]]                        # c input must also be null
+        self.c -= aux * self.A[i,:]                               # Substract all the vector c
+        self.optimal_solution -= aux * self.b[i]                  # Substract the optimal value with respect to vector b
+    vfunc = np.vectorize(rounding_to_zero)                        # Applies the function in parameter in all the vector's input
+    self.A = vfunc(self.A)                                        # (set very low values to zero)
     self.b = vfunc(self.b)
     self.c = vfunc(self.c)
     self.optimal_solution = vfunc(self.optimal_solution)
+
+
+c = [1,2,3,4]
+A = [[2,5,3,4],
+     [3,2,4,9],
+     [4,3,2,5]]
+b = [10,15,20,25]
+
+teste = Tableau()
+teste.c = np.array(c, dtype = float)
+teste.A = np.array(A, dtype = float)
+teste.b = np.array(b, dtype = float)
+teste.base_columns = [1,2,3]
+teste.dimension = (3,4)
+
+canonical_form(teste)
+print(teste.A)
+
